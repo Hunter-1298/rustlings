d328737e6b1d67ee4c4efd0faf23fbafb9467d0f
Hunter Hayden <hunterhayden@MacBook-Pro-3.local>
2025-07-12 00:04:36 -0400

    quarter through the rustlings

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -1,13 +1,13 @@
 fn animal_habitat(animal: &str) -> &str {
     // TODO: Fix the compiler error in the statement below.
-    let identifier = if animal == "crab" {
+    let identifier: i32 = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        4
     };
 
     // Don't change the expression below!
@@ -24,6 +24,9 @@ fn animal_habitat(animal: &str) -> &str {
 
 fn main() {
     // You can optionally experiment here.
+    let num: i32 = 10;
+    let n: &i32 = &num;
+    println!("{}", n)
 }
 
 // Don't change the tests!
