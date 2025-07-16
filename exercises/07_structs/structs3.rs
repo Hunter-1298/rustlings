d423bc1d27968c1436c8c87e0633c6a92cdcbadd
Hunter Hayden <hunterhayden@MacBook-Pro-3.local>
2025-07-16 01:06:15 -0400

    35% complete

--- exercises/07_structs/structs3.rs
+++ exercises/07_structs/structs3.rs
@@ -24,14 +24,14 @@ impl Package {
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn is_international(&self) {
-        // TODO: Read the tests that use this method to find out when a package
-        // is considered international.
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
     // TODO: Add the correct return type to the function signature.
-    fn get_fees(&self, cents_per_gram: u32) {
+    fn get_fees(&self, cents_per_gram: u32) -> u32 {
         // TODO: Calculate the package's fees.
+        self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -76,9 +76,7 @@ mod tests {
     fn calculate_transport_fees() {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
-
         let cents_per_gram = 3;
-
         let package = Package::new(sender_country, recipient_country, 1500);
 
         assert_eq!(package.get_fees(cents_per_gram), 4500);
